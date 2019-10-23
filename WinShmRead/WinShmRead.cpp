c0725c7d1ce60146653ae0cfb2e4b010f8fd9bbd
dancer <huzhigen@bytedance.com>
2019-10-23 21:25:57 +0800

    window共享内存代码

---
+++ WinShmRead/WinShmRead.cpp
@@ -0,0 +1,37 @@
+﻿// WinShmRead.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>  
+#include <windows.h>  
+using namespace std;
+
+#define BUF_SIZE 4096
+
+int main()
+{
+	// 打开共享的文件对象
+	HANDLE hMapFile = OpenFileMapping(FILE_MAP_ALL_ACCESS, NULL, L"ShareMemory");
+	if (hMapFile)
+	{
+		LPVOID lpBase = MapViewOfFile(hMapFile, FILE_MAP_ALL_ACCESS, 0, 0, 0);
+		// 将共享内存数据拷贝出来
+		char szBuffer[BUF_SIZE] = { 0 };
+		strcpy(szBuffer, (char*)lpBase);
+		printf("%s", szBuffer);
+
+		// 解除文件映射
+		UnmapViewOfFile(lpBase);
+		// 关闭内存映射文件对象句柄
+		CloseHandle(hMapFile);
+	}
+	else
+	{
+		// 打开共享内存句柄失败
+		printf("OpenMapping Error");
+	}
+	std::cin.get();
+	return 0;
+}
+
+
---
+++ WinShmWrite/WinShmWrite.cpp
@@ -0,0 +1,51 @@
+﻿// WinShmWrite.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+#include "stdafx.h"
+#include <windows.h>
+#include <iostream>
+using namespace std;
+
+#define BUF_SIZE 4096
+
+int _tmain(int argc, TCHAR* argv[], TCHAR* envp[])
+{
+	// 定义共享数据
+	char szBuffer[] = "____ 世界上最好的语言?";
+
+	// 创建共享文件句柄 
+	HANDLE hMapFile = CreateFileMapping(
+		INVALID_HANDLE_VALUE,   // 物理文件句柄
+		NULL,   // 默认安全级别
+		PAGE_READWRITE,   // 可读可写
+		0,   // 高位文件大小
+		BUF_SIZE,   // 地位文件大小
+		L"ShareMemory"   // 共享内存名称
+	);
+
+	// 映射缓存区视图 , 得到指向共享内存的指针
+	LPVOID lpBase = MapViewOfFile(
+		hMapFile,            // 共享内存的句柄
+		FILE_MAP_ALL_ACCESS, // 可读写许可
+		0,
+		0,
+		BUF_SIZE
+	);
+
+	// 将数据拷贝到共享内存
+	strcpy((char*)lpBase, szBuffer);
+	std::cout << "Write Finish, waiting ..." << std::endl;
+	// 线程挂起等其他线程读取数据
+	Sleep(20000);
+
+	// 解除文件映射
+	UnmapViewOfFile(lpBase);
+	// 关闭内存映射文件对象句柄
+	CloseHandle(hMapFile);
+	std::cin.get();
+	return 0;
+}
+
